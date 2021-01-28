5d1bdb355f3061fe122a5cdca8702619d7ca0a76
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-01-28 16:39:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+var time = new Date().getHours();
+var messageText;
+var noon = 12;
+var evening = 18; // 6PM
+var wakeupTime = 9; // 9AM
+var lunchTime = 12; // 12PM
+var partyTime = 17; // 5PM
+var napTime = lunchTime + 2; // 2PM
+
+var updateClock = function() 
+{
+	var lolcat = document.getElementById('lolcat');
+	var message = document.getElementById('timeEvent');
+	var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+
+	if (time == partyTime){
+		messageText = "IZ PARTEE TIME!!";
+		image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
+	} 
+	else if (time == napTime) {
+		messageText = "IZ NAP TIME...";
+		image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg" ;
+	} 
+	else if (time == lunchTime) {
+		messageText = "IZ NOM NOM NOM TIME!!";
+		image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+	} 
+	else if (time == wakeupTime) {
+		messageText = "IZ TIME TO GETTUP.";
+		image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+	} 
+	else if (time < noon) {
+		messageText = "Good morning!";
+	} 
+	else if (time > evening) {
+		messageText = "Good Evening!";
+	} 
+	else {
+		messageText = "Good afternoon!";
+	}
+
+	message.innerText = messageText;
+	lolcat.src = image;
+	
+	showCurrentTime();
+};
+
+var showCurrentTime = function()
+	{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+ 
+    var currentTime = new Date();
+ 
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+ 
+    // Set hours 
+    if (hours >= noon) 
+    { 
+        meridian = "PM"; 
+    }  
+    if (hours > noon) 
+    { 
+        hours = hours - 12; 
+    }
+ 
+    // Set Minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+ 
+    // Set Seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+ 
+    // put together the string that displays the time
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+ 
+    clock.innerText = clockTime;
+};
+
+updateClock();
+
+var oneSecond = 1000;
+setInterval(updateClock, oneSecond);
