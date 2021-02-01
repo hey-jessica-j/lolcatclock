6e618900425f69c9dfa3b0f5a148ecb8082fc9fe
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-01-31 20:02:09 -0500

    added button

--- script.js
+++ script.js
@@ -89,3 +89,22 @@ updateClock();
 
 var oneSecond = 1000;
 setInterval(updateClock, oneSecond);
+
+var isPartyTime = false;
+
+var partyEvent = function () 
+{
+	if (isPartyTime === false)
+	{	isPartyTime = true;
+		time = partyTime;
+		partyTimeButton.innerText = "PARTY TIME";
+		partyTimeButton.style.backgroundColor = "#222";}
+		else {
+		isPartyTime = false;
+		time = new Date().getHours();
+			partyTimeButton.innerText = "Party Over";
+		partyTimeButton.style.backgroundColor = "#0a8DAB";
+			}
+};
+
+partyTimeButton.addEventListener("click", partyEvent);
